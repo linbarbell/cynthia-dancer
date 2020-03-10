ae0abcdbd62d6d07f2458189bc43f230f2a3d555
Clay Harris <rcharris@email.wm.edu>
2020-03-10 16:57:39 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,13 +13,35 @@ client.on('ready', () => {
  
 
 client.on('message', message => {
+    if(message.author.bot) return;
+    if (message.content.includes('Yas')) {
+        message.channel.send('yas!');   
+    }
+  
+});
 
-    if (message.content === 'ping') {
+client.on('message', message => {
+    if(message.author.bot) return;
+    if(message.content.includes('yas')) {
+        message.channel.send('yas!');   
+    }
+  
+});
 
-       message.reply('pong');
+client.on('message', message => {
+    if(message.author.bot) return;
+    if(message.content.includes('Cynthia')) {
+        message.channel.send("Ready? Here we go now! Cynthia she's a really cool dancer. Cynthia boogie to the groove now. She's got those moves, long as you move those arms and legs. She's moving up on the floor. She's ready to break some eggs. Make an omlette Cynthia. Whoo! Shake it, shake it. You're so fantastic. Just don't break it, it's only made of plastic. Cynthia she's a real cool dancer. Cynthia boogie to the groove.");   
+    }
+  
+});
 
-       }
-
+client.on('message', message => {
+    if(message.author.bot) return;
+    if(message.content.includes('cynthia')) {
+        message.channel.send("Ready? Here we go now! Cynthia she's a really cool dancer. Cynthia boogie to the groove now. She's got those moves, long as you move those arms and legs. She's moving up on the floor. She's ready to break some eggs. Make an omlette Cynthia. Whoo! Shake it, shake it. You're so fantastic. Just don't break it, it's only made of plastic. Cynthia she's a real cool dancer. Cynthia boogie to the groove.");   
+    }
+  
 });
 
  
