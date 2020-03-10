843619537d028bbf9f16d48e08ddbfceec6334a9
Clay Harris <rcharris@email.wm.edu>
2020-03-10 17:45:49 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,13 +5,11 @@ const client = new Discord.Client();
  
 
 client.on('ready', () => {
-console.log('Bot: Hosting ' + `${client.users.size}` + ' users, in ' + `${client.channels.size}` + ' channels of ' + `${client.guilds.size}` + ' guilds.');
     client.user.setStatus('online')
     client.user.setPresence({
         game: {
             name: 'me dance',
-            type: '3',
-            url: 'https://discordapp.com/'
+            type: '3'
         }
     });
 });
