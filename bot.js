fe19fe6f335c4529298dac4d723eb88060ac590a
Clay Harris <rcharris@email.wm.edu>
2020-03-10 15:59:35 -0600

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,40 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login('NTczOTMwNTQxMjg0MzI3NDI0.XmfnGA.qSlWicwxBg9OUtJNtIo5CwEdSvU');
+
+client.on('message', message => {
+    if(message.author.bot) return;
+    if (message.content.includes('Yas')) {
+        message.channel.send('yas!');   
+    }
+  
+});
+
+client.on('message', message => {
+    if(message.author.bot) return;
+    if(message.content.includes('yas')) {
+        message.channel.send('yas!');   
+    }
+  
+});
+
+client.on('message', message => {
+    if(message.author.bot) return;
+    if(message.content.includes('Cynthia')) {
+        message.channel.send("Ready? Here we go now! Cynthia she's a really cool dancer. Cynthia boogie to the groove now. She's got those moves, long as you move those arms and legs. She's moving up on the floor. She's ready to break some eggs. Make an omlette Cynthia. Whoo! Shake it, shake it. You're so fantastic. Just don't break it, it's only made of plastic. Cynthia she's a real cool dancer. Cynthia boogie to the groove.");   
+    }
+  
+});
+
+client.on('message', message => {
+    if(message.author.bot) return;
+    if(message.content.includes('cynthia')) {
+        message.channel.send("Ready? Here we go now! Cynthia she's a really cool dancer. Cynthia boogie to the groove now. She's got those moves, long as you move those arms and legs. She's moving up on the floor. She's ready to break some eggs. Make an omlette Cynthia. Whoo! Shake it, shake it. You're so fantastic. Just don't break it, it's only made of plastic. Cynthia she's a real cool dancer. Cynthia boogie to the groove.");   
+    }
+  
+});
