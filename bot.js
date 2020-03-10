27be999eab5e563a7ceb96a86770b904ef5f3427
Clay Harris <rcharris@email.wm.edu>
2020-03-10 17:43:08 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ const client = new Discord.Client();
 client.on('ready', () => {
 console.log('Bot: Hosting ' + `${client.users.size}` + ' users, in ' + `${client.channels.size}` + ' channels of ' + `${client.guilds.size}` + ' guilds.');
     client.user.setStatus('online')
-    client.user.setActivity({
+    client.user.setPresence({
         game: {
             name: 'me dance',
             type: '3',
