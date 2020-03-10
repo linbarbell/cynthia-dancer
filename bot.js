11d29bcf0a19a5c0e9109f3d2696e7ffdbe39b62
Clay Harris <rcharris@email.wm.edu>
2020-03-10 17:21:48 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setActivity('YouTube', { type: 'WATCHING' });
+  client.user.setActivity('me dance', { type: 'WATCHING' });
 })
 
  
