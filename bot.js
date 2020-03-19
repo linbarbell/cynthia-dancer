bff1aa261aa8c1bbd382f85d509805c70e9b8028
Clay Harris <rcharris@email.wm.edu>
2020-03-19 16:02:21 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,7 +27,7 @@ client.on('message', message => {
 
 client.on('message', message => {
     if(message.author.bot) return;
-    if (message.content.toLowerCase() == ('grats*')) {
+    if (message.content.toLowerCase() == ('grats***')) {
         message.channel.send('Congratulations!');   
     }
   
@@ -35,7 +35,7 @@ client.on('message', message => {
 
 client.on('message', message => {
     if(message.author.bot) return;
-    if (message.content.toLowerCase() == ('gratz*')) {
+    if (message.content.toLowerCase() == ('gratz***')) {
         message.channel.send('Congratulations!');   
     }
   
