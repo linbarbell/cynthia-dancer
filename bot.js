91d1d55953bc9d0826f5d2f9e8f50ab6ffa38532
Clay Harris <rcharris@email.wm.edu>
2020-03-19 19:07:49 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
 client.on('message', message => {
     if (message.author.bot) return;
     let content = message.content.toLowerCase();
-    if (content.search(/\b(c+o+n+g+r+a+)\b/) !== -1 return;
+    if (content.search(/\b(c+o+n+g+r+a+)\b/) !== -1) return;
     if (content.search(/\b(g+z+)+\b/) !== -1 || content.search(/\b(c+o+n+g+r+a+.+)\b/) !== -1 || content.includes('grats') || content.includes('gratz')) {
         message.channel.send('Congratulations!')
     }
