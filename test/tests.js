b10f288fed7d3363536e40b333eb4e16ebc18a80
Emmett Lin <lin.emmett93@gmail.com>
2020-03-19 23:18:07 -0400

    make unit testable and add unit tests

--- test/tests.js
+++ test/tests.js
@@ -28,6 +28,55 @@ describe('gz', function() {
         assert.equal(1, cynResponses.length);
         assert.equal(responses.congratulations, cynResponses[0]);
     });
+    it('should not congrats on zigzag', function() {
+        let cynResponses = responses.get('zigzag');
+        assert.equal(0, cynResponses.length);
+    });
+
+    it('should congrats on congratulations', function() {
+        let cynResponses = responses.get('congratulations');
+        assert.equal(1, cynResponses.length);
+        assert.equal(responses.congratulations, cynResponses[0]);
+    });
+    it('should congrats on congraaaaaaaats', function() {
+        let cynResponses = responses.get('congraaaaaaaats');
+        assert.equal(1, cynResponses.length);
+        assert.equal(responses.congratulations, cynResponses[0]);
+    });
+    it('should congrats on congrabulations', function() {
+        let cynResponses = responses.get('congrabulations');
+        assert.equal(1, cynResponses.length);
+        assert.equal(responses.congratulations, cynResponses[0]);
+    });
+    it('should not congrats on congra', function() {
+        let cynResponses = responses.get('congra');
+        assert.equal(0, cynResponses.length);
+    });
+    it('should not congrats on congraa', function() {
+        let cynResponses = responses.get('congraa');
+        assert.equal(0, cynResponses.length);
+    });
+
+    it('should congrats on grats', function() {
+        let cynResponses = responses.get('grats');
+        assert.equal(1, cynResponses.length);
+        assert.equal(responses.congratulations, cynResponses[0]);
+    });
+    it('should congrats on gratz', function() {
+        let cynResponses = responses.get('gratz');
+        assert.equal(1, cynResponses.length);
+        assert.equal(responses.congratulations, cynResponses[0]);
+    });
+    it('should congrats on gratzz', function() {
+        let cynResponses = responses.get('gratzz');
+        assert.equal(1, cynResponses.length);
+        assert.equal(responses.congratulations, cynResponses[0]);
+    });
+    it('should congrats on gratss', function() {
+        let cynResponses = responses.get('gratss');
+        assert.equal(1, cynResponses.length);
+        assert.equal(responses.congratulations, cynResponses[0]);
+    });
 
     it('should not congrats multiple times', function() {
         let cynResponses = responses.get('gzgz congratulations grats gzzzzzzz!');
